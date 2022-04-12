e61ce3009c0f09dfa5612393e6b0677f66bc6476
hh15248 <heather.hannan.524@gmail.com>
2022-04-12 09:54:42 -0500

    Final3

--- my_demo/contact/views.py
+++ my_demo/contact/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from .forms import FriendForm
 
 # Create your views here.
-def contact_view(request, *args, **kwargs):
-    my_context = {
-        "my_text" : "Hi my name is Bevo and I am a very good longhorn"
-    }
-    return render(request, "contact.html", my_context)
-
 def contact_view(request):
     form = FriendForm(request.POST or None)
     if form.is_valid():
